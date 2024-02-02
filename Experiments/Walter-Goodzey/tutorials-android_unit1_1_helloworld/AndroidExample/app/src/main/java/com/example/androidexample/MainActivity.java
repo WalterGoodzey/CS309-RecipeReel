35fb5e9d4f1b295e9d6693f6458466eca8471a54
Walter Goodzey <wgoodzey@iastate.edu>
2024-02-02 10:16:33 -0600

    changed color of text output as well as changed output message

--- Experiments/Walter-Goodzey/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/Walter-Goodzey/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
+        messageText.setTextColor(Color.RED);
         messageText.setText("Hello Hossain, this is a message from Walter :)");
 
     }
