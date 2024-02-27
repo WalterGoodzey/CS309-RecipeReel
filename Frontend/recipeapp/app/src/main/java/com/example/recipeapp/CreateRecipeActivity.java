f20adb9c263ede8a1b3a1f0af6080b6dc1385cbe
Walter Goodzey <wgoodzey@iastate.edu>
2024-02-27 00:30:51 -0600

    created CreateRecipeActivity as well as its activity xml

---
+++ Frontend/recipeapp/app/src/main/java/com/example/recipeapp/CreateRecipeActivity.java
@@ -0,0 +1,11 @@
+package com.example.recipeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class CreateRecipeActivity {
+}
