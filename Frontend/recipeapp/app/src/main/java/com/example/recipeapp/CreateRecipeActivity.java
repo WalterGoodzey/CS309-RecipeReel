d418f911070494e241ed9662be2e22a97ab3dbc5
Walter Goodzey <wgoodzey@iastate.edu>
2024-02-28 16:32:41 -0600

    Fixed layout of xml file as well as fixed the gradle build so it implements volley

--- Frontend/recipeapp/app/src/main/java/com/example/recipeapp/CreateRecipeActivity.java
+++ Frontend/recipeapp/app/src/main/java/com/example/recipeapp/CreateRecipeActivity.java
@@ -2,6 +2,27 @@ package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -13,6 +34,6 @@ public class CreateRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe);
-        
+
     }
 }
