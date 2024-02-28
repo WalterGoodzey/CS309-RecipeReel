7652b0acab36da373d2ca1f8ecae4fabd2a7ece5
willi <wpcustis@iastate.edu>
2024-02-28 15:35:21 -0600

    Minor changes

--- Backend/backend/src/main/java/recipeapp/Trending/TrendingRepository.java
+++ Backend/backend/src/main/java/recipeapp/Trending/TrendingRepository.java
@@ -1,4 +1,17 @@
 package recipeapp.Trending;
 
-public class TrendingRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Will Custis
+ *
+ */
+
+public interface TrendingRepository extends JpaRepository<Trending, Long>{
+    Trending findById(int trendingID);
+
+    void deleteById(int trendingID);
+
+    Trending findByRecipe_Id(int id);
 }
