66f14f55a62c7a53208db049660258b673e0fc62
ryanmcf7 <ryanmcf7@iastate.edu>
2024-02-29 11:14:07 -0600

    fixed errors with profile get request, changed activities to send id with intent instead of username

--- Frontend/recipeapp/app/src/main/java/com/example/recipeapp/ProfileActivity.java
+++ Frontend/recipeapp/app/src/main/java/com/example/recipeapp/ProfileActivity.java
@@ -36,11 +36,8 @@ public class ProfileActivity extends AppCompatActivity {
     private Button logoutButton;
     private Button entryButton;
     private String username;
-    private JSONObject user;
     private int userId;
-
     private String URL_GET_USER_BASE = "http://coms-309-018.class.las.iastate.edu:8080/users";
-
     private String URL_GET_USER;
 
     @Override
@@ -55,31 +52,14 @@ public class ProfileActivity extends AppCompatActivity {
         logoutButton = findViewById(R.id.profile_logout_btn);
         entryButton = findViewById(R.id.profile_entry_btn);
 
-        //see if user is logged in and setup the screen accordingly
-//        Bundle extras = getIntent().getExtras();
-//        if(extras == null){
-//            usernameText.setText("No user signed in");
-//            logoutButton.setVisibility(View.INVISIBLE);
-//        } else {
-//            username = extras.getString("USERNAME");
-//            usernameText.setText(username);
-//        }
         Intent createIntent = getIntent();
-        /* if intent's USERNAME is not null, show user's profile page. If not, user is not signed in*/
-        if(createIntent.hasExtra("USERNAME") && createIntent.getStringExtra("USERNAME") != null){
-            username = createIntent.getStringExtra("USERNAME");
+        /* if intent has id and it is not null or the default value of -1, show user's profile page. If not, user is not signed in*/
+        if(createIntent.hasExtra("id") && createIntent.getIntExtra("id", -1) > 0){
+
+            /* set userId and add to url for GET request, make GET request (fills in user's profile page) */
             userId = createIntent.getIntExtra("id", -1);
             URL_GET_USER = URL_GET_USER_BASE + "/" + userId;
             getUserInfoReq();
-
-//            usernameText.setText(username);
-            try{
-                usernameText.setText(user.getString("username"));
-                descriptionText.setText(user.getString("emailAddress"));
-            } catch (JSONException e) {
-                Toast.makeText(getApplicationContext(), "JSONException trying to set text", Toast.LENGTH_LONG).show();
-                throw new RuntimeException(e);
-            }
 
             guestText.setVisibility(View.INVISIBLE);
             entryButton.setVisibility(View.INVISIBLE);
@@ -119,22 +99,22 @@ public class ProfileActivity extends AppCompatActivity {
             int id = item.getItemId();
             if (id == R.id.bottom_trending) {
                 Intent intent = new Intent(getApplicationContext(), TrendingActivity.class);
-                intent.putExtra("USERNAME", username);
+                intent.putExtra("id", userId);
                 startActivity(intent);
                 return true;
             } else if (id == R.id.bottom_following) {
                 Intent intent = new Intent(getApplicationContext(), FollowingActivity.class);
-                intent.putExtra("USERNAME", username);
+                intent.putExtra("id", userId);
                 startActivity(intent);
                 return true;
             } else if (id == R.id.bottom_search) {
                 Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
-                intent.putExtra("USERNAME", username);
+                intent.putExtra("id", userId);
                 startActivity(intent);
                 return true;
             } else if (id == R.id.bottom_saved) {
                 Intent intent = new Intent(getApplicationContext(), SavedActivity.class);
-                intent.putExtra("USERNAME", username);
+                intent.putExtra("id", userId);
                 startActivity(intent);
                 return true;
             } else if (id == R.id.bottom_profile) {
@@ -146,7 +126,7 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     private void getUserInfoReq() {
-        user = new JSONObject();
+//        user = new JSONObject();
         JsonObjectRequest userReq = new JsonObjectRequest(Request.Method.GET,
                 URL_GET_USER,
                 null, // Pass null as the request body since it's a GET request
@@ -154,29 +134,18 @@ public class ProfileActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         Log.d("Volley Response", response.toString());
-                        Toast.makeText(getApplicationContext(), "Volley Received Response", Toast.LENGTH_LONG).show();
-                        //for testing only
-                        int idTest = 3;
-                        String usernameTest = "testusername";
-                        String emailAddressTest = "testemail";
-                        String passwordTest = "testpassword";
-                        try {
-                            idTest = response.getInt("id");
-                            usernameTest = response.getString("username");
-                            emailAddressTest = response.getString("emailAddress");
-                            passwordTest = response.getString("password");
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
-                        try {
-                            user.put("id", idTest);
-                            user.put("username", usernameTest);
-                            user.put("emailAddress", emailAddressTest);
-                            user.put("password", passwordTest);
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
+//                        Toast.makeText(getApplicationContext(), "Volley Received Response", Toast.LENGTH_LONG).show();
 
+                        String usernameResponse = "testusername";
+                        String emailResponse = "testemail";
+                        try{
+                            usernameResponse = response.getString("username");
+                            emailResponse = response.getString("emailAddress");
+                        }catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                        usernameText.setText(usernameResponse);
+                        descriptionText.setText(emailResponse);
                     }
                 },
                 new Response.ErrorListener() {
@@ -203,7 +172,7 @@ public class ProfileActivity extends AppCompatActivity {
             }
         };
 
-        Toast.makeText(getApplicationContext(), "Adding request to Volley Queue", Toast.LENGTH_LONG).show();
+//        Toast.makeText(getApplicationContext(), "Adding request to Volley Queue", Toast.LENGTH_LONG).show();
         // Adding request to request queue
         VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(userReq);
     }
