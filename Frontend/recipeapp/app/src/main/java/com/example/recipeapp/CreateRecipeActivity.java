038f79aa8efca1b7a83b7de3bc4fdb836711555a
Walter Goodzey <wgoodzey@iastate.edu>
2024-03-01 14:26:55 -0600

    round trip success

--- Frontend/recipeapp/app/src/main/java/com/example/recipeapp/CreateRecipeActivity.java
+++ Frontend/recipeapp/app/src/main/java/com/example/recipeapp/CreateRecipeActivity.java
@@ -35,7 +35,7 @@ public class CreateRecipeActivity extends AppCompatActivity {
 
     private TextView server_response;
     private static final String URL_JSON_OBJ =
-            "http://coms-309-018.class.las.iastate.edu:8080/recipe";
+            "http://coms-309-018.class.las.iastate.edu:8080/recipes";
 //            "https://jsonplaceholder.typicode.com/users/1";
 
     @Override
