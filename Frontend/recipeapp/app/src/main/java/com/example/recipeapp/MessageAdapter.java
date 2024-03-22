11ea7e954ea942b95d6c8ba8a0d8219eb991ad4e
ryanmcf7 <ryanmcf7@iastate.edu>
2024-03-22 18:21:40 -0500

    fixed crashing on scrolling, chat is successful at sending messages and receiving echo from local websocket

--- Frontend/recipeapp/app/src/main/java/com/example/recipeapp/MessageAdapter.java
+++ Frontend/recipeapp/app/src/main/java/com/example/recipeapp/MessageAdapter.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
 import java.util.List;
 
 /**
@@ -22,31 +25,85 @@ public class MessageAdapter extends ArrayAdapter<MessageItemObject> {
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
         MessageItemObject item = getItem(position);
+        if(item.getSendingMessage()){
+            //message is sent by the user
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.message_item, parent, false);
+            }
 
-        // Check if an existing view is being reused, otherwise inflate the view
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.message_item, parent, false);
+            // Lookup view for data population
+            TextView itemDate = convertView.findViewById(R.id.text_chat_date);
+            TextView itemMessageSent = convertView.findViewById(R.id.text_chat_message_sent);
+            TextView itemTimestampSent = convertView.findViewById(R.id.text_chat_timestamp_sent);
+            TextView itemUserReceived = convertView.findViewById(R.id.text_chat_user_received);
+            TextView itemMessageReceived = convertView.findViewById(R.id.text_chat_message_received);
+            TextView itemTimestampReceived = convertView.findViewById(R.id.text_chat_timestamp_received);
+            CardView cardSent = convertView.findViewById(R.id.card_chat_message_sent);
+            CardView cardReceived = convertView.findViewById(R.id.card_chat_message_received);
+
+            // Populate the data into the template view using the data object
+            //populate sent message & timestamp
+            itemDate.setText(item.getDate());
+            itemMessageSent.setText(item.getMessage());
+            itemTimestampSent.setText(item.getTimestamp());
+
+            //view sent message block
+            itemMessageSent.setVisibility(View.VISIBLE);
+            itemTimestampSent.setVisibility(View.VISIBLE);
+            cardSent.setVisibility(View.VISIBLE);
+
+            //hide received message block
+            itemUserReceived.setVisibility(View.INVISIBLE);
+            itemMessageReceived.setVisibility(View.INVISIBLE);
+            itemTimestampReceived.setVisibility(View.INVISIBLE);
+            cardReceived.setVisibility(View.INVISIBLE);
+
+
+            // Return the completed view to render on screen
+            return convertView;
+
+        } else {
+            //message is being received by the user
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.message_item, parent, false);
+            }
+
+            // Lookup view for data population
+            TextView itemDate = convertView.findViewById(R.id.text_chat_date);
+            TextView itemMessageSent = convertView.findViewById(R.id.text_chat_message_sent);
+            TextView itemTimestampSent = convertView.findViewById(R.id.text_chat_timestamp_sent);
+            TextView itemUserReceived = convertView.findViewById(R.id.text_chat_user_received);
+            TextView itemMessageReceived = convertView.findViewById(R.id.text_chat_message_received);
+            TextView itemTimestampReceived = convertView.findViewById(R.id.text_chat_timestamp_received);
+            CardView cardSent = convertView.findViewById(R.id.card_chat_message_sent);
+            CardView cardReceived = convertView.findViewById(R.id.card_chat_message_received);
+
+
+            // Populate the data into the template view using the data object
+            String tempUser = "user: " + item.getSenderId();
+            itemUserReceived.setText(tempUser); //TODO - update to get sender's username
+
+            //populate received user, message, & timestamp
+            itemMessageReceived.setText(item.getMessage());
+            itemTimestampReceived.setText(item.getTimestamp());
+            itemDate.setText(item.getDate());
+
+            //view received message block
+            itemUserReceived.setVisibility(View.VISIBLE);
+            itemMessageReceived.setVisibility(View.VISIBLE);
+            itemTimestampReceived.setVisibility(View.VISIBLE);
+            cardReceived.setVisibility(View.VISIBLE);
+
+            //hide sent message block
+            itemMessageSent.setVisibility(View.INVISIBLE);
+            itemTimestampSent.setVisibility(View.INVISIBLE);
+            cardSent.setVisibility(View.INVISIBLE);
+
+            // Return the completed view to render on screen
+            return convertView;
         }
-
-        // Lookup view for data population
-        TextView itemDate = convertView.findViewById(R.id.text_chat_date);
-        TextView itemMessageSent = convertView.findViewById(R.id.text_chat_message_sent);
-        TextView itemTimestampSent = convertView.findViewById(R.id.text_chat_timestamp_sent);
-        TextView itemUserReceived = convertView.findViewById(R.id.text_chat_user_received);
-        TextView itemMessageReceived = convertView.findViewById(R.id.text_chat_message_received);
-        TextView itemTimestampReceived = convertView.findViewById(R.id.text_chat_timestamp_received);
-
-        // Populate the data into the template view using the data object
-        itemDate.setText(item.getDate());
-        itemMessageSent.setText(item.getMessage());
-        itemTimestampSent.setText(item.getTimestamp());
-        itemUserReceived.setText(item.getOtherUser());
-        //populate sent and received message & timestamp with the same data, hide one later
-        itemMessageReceived.setText(item.getMessage());
-        itemTimestampReceived.setText(item.getTimestamp());
-
-        // Return the completed view to render on screen
-        return convertView;
     }
 
 //    @Override
--- Frontend/recipeapp/app/src/main/java/com/example/recipeapp/MessageItemObject.java
+++ Frontend/recipeapp/app/src/main/java/com/example/recipeapp/MessageItemObject.java
@@ -5,27 +5,25 @@ package com.example.recipeapp;
  */
 public class MessageItemObject {
     private String message;
-    private String otherUser;
     private String date;
     private String timestamp;
     private int senderId;
+    private Boolean sendingMessage;
 
-
-    public MessageItemObject(String message, String otherUser, String date, String timestamp, int senderId) {
+    public MessageItemObject(String message, String date, String timestamp, int senderId, Boolean sendingMessage) {
         this.message = message;
-        this.otherUser = otherUser;
         this.date = date;
         this.timestamp = timestamp;
         this.senderId = senderId;
+        this.sendingMessage = sendingMessage;
     }
 
     public String getMessage() { return message; }
-
-    public String getOtherUser() { return otherUser; }
 
     public String getDate() { return date; }
 
     public String getTimestamp() { return timestamp; }
 
     public int getSenderId(){ return senderId; }
+    public Boolean getSendingMessage(){ return sendingMessage; }
 }
