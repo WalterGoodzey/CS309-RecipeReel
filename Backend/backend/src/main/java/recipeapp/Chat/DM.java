14456d4e41dfe80c41194f244e33969cb3760b08
David Borucki <02davidb@gmail.com>
2024-03-27 14:21:48 -0500

    DM and DM Repository

---
+++ Backend/backend/src/main/java/recipeapp/Chat/DM.java
@@ -0,0 +1,28 @@
+package recipeapp.Chat;
+import java.util.Date;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DM {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String username;
+    @Lob
+    private String content;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="sent")
+    private Date sent = new Date();
+
+    public DM(){};
+
+    public DM(String username, String content) {
+        this.username = username;
+        this.content = content;
+    };
+
+}
---
+++ Backend/backend/src/main/java/recipeapp/Chat/DMRepository.java
@@ -0,0 +1,5 @@
+package recipeapp.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface DMRepository extends JpaRepository<DM, Long> {
+
+}
