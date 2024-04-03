a2c7e8f2c79288e44fec4982da3938433e7f3870
David Borucki <02davidb@gmail.com>
2024-04-03 16:07:08 -0500

    got rid of that dogwater of javadocs

--- Backend/backend/javadocs-backend/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"recipeapp.Chat"},{"l":"recipeapp.Recipes"},{"l":"recipeapp.TrendingRecipe"},{"l":"recipeapp.Users"}];updateSearchResults();
