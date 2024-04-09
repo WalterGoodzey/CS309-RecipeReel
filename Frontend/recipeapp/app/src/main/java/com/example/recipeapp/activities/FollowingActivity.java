ef6737bc592fb55ebd484c6bcd395d0dba4f8566
Walter Goodzey <wgoodzey@iastate.edu>
2024-04-09 14:44:53 -0500

    copied makeRecipeListReq from Trending

--- Frontend/recipeapp/app/src/main/java/com/example/recipeapp/activities/FollowingActivity.java
+++ Frontend/recipeapp/app/src/main/java/com/example/recipeapp/activities/FollowingActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -11,12 +12,23 @@ import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.recipeapp.R;
+import com.example.recipeapp.VolleySingleton;
 import com.example.recipeapp.adapters.RecipeAdapter;
 import com.example.recipeapp.objects.RecipeItemObject;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Activity to display recent recipes published by users that the
@@ -30,6 +42,8 @@ public class FollowingActivity extends AppCompatActivity {
     /** ListView to store list of RecipeItemObjects */
     private ListView listView;
 
+    /** Server link*/
+    private String SERVER_URL = "http://coms-309-018.class.las.iastate.edu:8080/";
     /**
      * onCreate method for FollowingActivity
      *
@@ -115,4 +129,59 @@ public class FollowingActivity extends AppCompatActivity {
             adapter.add(item);
         }
     }
+
+    private void makeRecipeListReq() {
+        JsonArrayRequest recipeListReq = new JsonArrayRequest(
+                Request.Method.GET,
+                SERVER_URL + "saved/" + userId,
+                null, // Pass null as the request body since it's a GET request
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("Volley Response", response.toString());
+
+                        // Parse the JSON array and add data to the adapter
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                int recipeId = jsonObject.getInt("id");
+                                String title = jsonObject.getString("title");
+                                String author = jsonObject.getString("username");
+                                String description = jsonObject.getString("description");
+
+                                // Create a ListItemObject and add it to the adapter
+                                RecipeItemObject item = new RecipeItemObject(recipeId, title, author, description, jsonObject);
+                                adapter.add(item);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley Error", error.toString());
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(recipeListReq);
+    }
+
 }
