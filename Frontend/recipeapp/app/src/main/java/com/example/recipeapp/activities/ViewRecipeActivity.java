4a392f365c9c8c72a018a146d6ac7e4d191493b2
Walter Goodzey <wgoodzey@iastate.edu>
2024-04-16 15:06:38 -0500

    Added some documentation for new objects as well as added TODO messages

--- Frontend/recipeapp/app/src/main/java/com/example/recipeapp/activities/ViewRecipeActivity.java
+++ Frontend/recipeapp/app/src/main/java/com/example/recipeapp/activities/ViewRecipeActivity.java
@@ -65,12 +65,12 @@ public class ViewRecipeActivity extends AppCompatActivity {
      */
     private int recipeId;
     /**
-     * Button for giving recipe a 1star rating - TEMPORARY
+     * Buttons for giving recipe a ratings as well as cancel rating process
      */
     private Button rate1, rate2, rate3, rate4, rate5, cancelRate;
 
     private String URL_SERVER = "http://coms-309-018.class.las.iastate.edu:8080/";
-//    private String URL_SERVER = "recipeshttps://ae827564-7ce7-4ae9-bb71-dd282e411c72.mock.pstmn.io/recipes";
+//    private String URL_SERVER = "https://ae827564-7ce7-4ae9-bb71-dd282e411c72.mock.pstmn.io/recipes";
     /**
      * Specific URL for recipe rating requests
      */
@@ -80,9 +80,14 @@ public class ViewRecipeActivity extends AppCompatActivity {
      * Local user's userId
      */
     private int userId;
-
+    /**
+     * CardView to display popupCard
+     */
     private CardView popupCard;
-    private TextView popupMessage;
+    /**
+     * TextView to display rating prompt
+     */
+    private TextView popupRatingMessage;
 
     /**
      * onCreate for ViewRecipeActivity
@@ -102,7 +107,7 @@ public class ViewRecipeActivity extends AppCompatActivity {
         ingredientsTxt = findViewById(R.id.ingredientsText);
         instructionsTxt = findViewById(R.id.instructionsText);
         popupCard = findViewById(R.id.rate_recipe_popup_card);
-        popupMessage = findViewById(R.id.view_rate_popup);
+        popupRatingMessage = findViewById(R.id.view_rate_popup);
         //get recipeId from previous activity
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -110,7 +115,7 @@ public class ViewRecipeActivity extends AppCompatActivity {
         }
 
         popupCard.setVisibility(View.INVISIBLE);
-        popupMessage.setVisibility(View.INVISIBLE);
+        popupRatingMessage.setVisibility(View.INVISIBLE);
 
         rate1 = findViewById(R.id.rate1_button);
         rate2 = findViewById(R.id.rate2_button);
@@ -123,6 +128,134 @@ public class ViewRecipeActivity extends AppCompatActivity {
 
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.view_menu, menu);
+        return true;
+    }
+
+    /**
+     * Handler for when an option from the toolbar is selected
+     *
+     * @param item The menu item that was selected.
+     * @return true if applicable option is selected
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection.
+        int itemId = item.getItemId();
+        if (itemId == R.id.view_options_save_recipe) {
+            saveRecipe();
+            return true;
+        } else if (itemId == R.id.view_options_comments) {
+            // TODO - implement new activity to view comments
+            return true;
+        } else if (itemId == R.id.view_options_rate) {
+            rateRecipe();
+            return true;
+        } else if (itemId == R.id.view_options_view_author) {
+            // TODO - implement view author
+            return true;
+        } else if (itemId == R.id.view_options_chat_with_author) {
+            // TODO - implement chat with author
+            return true;
+            // need this for later viewing and making comments, updating recipe
+//        } else if (itemId == R.id.profile_options_logout) {
+//            /* when logout button is pressed, clear sharedPreferences (logging user out) and use intent to switch to Entry Activity */
+//            // getting the data which is stored in shared preferences.
+//            SharedPreferences saved_values = getSharedPreferences(getString(R.string.PREF_KEY), Context.MODE_PRIVATE);
+//            //make editor for shared preferences
+//            SharedPreferences.Editor editor = saved_values.edit();
+//            //clear and save shared preferences
+//            editor.clear();
+//            editor.apply();
+//            //go to EntryActivity
+////            startActivity(new Intent(ProfileActivity.this, EntryActivity.class));
+//            return true;
+//
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * TODO - add documentation for saveRecipe
+     */
+    private void saveRecipe() {
+        String url = URL_SERVER + "users/";
+        // TODO - implement save recipe
+    }
+
+    /**
+     * TODO - add documentation for rate Recipe
+     */
+    private void rateRecipe() {
+        popupCard.setVisibility(View.VISIBLE);
+        popupRatingMessage.setVisibility(View.VISIBLE);
+        /* click listener on rate1 button pressed */
+        rate1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* when rate button is pressed, use Volley request to rate the recipe at 1 star */
+                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 1;
+                popupCard.setVisibility(View.INVISIBLE);
+                popupRatingMessage.setVisibility(View.INVISIBLE);
+                putRecipeRating();
+            }
+        });
+        /* click listener on rate2 button pressed */
+        rate2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* when rate button is pressed, use Volley request to rate the recipe at 2 stars */
+                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 2;
+                popupCard.setVisibility(View.INVISIBLE);
+                popupRatingMessage.setVisibility(View.INVISIBLE);
+                putRecipeRating();
+            }
+        });
+        /* click listener on rate3 button pressed */
+        rate3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* when rate button is pressed, use Volley request to rate the recipe at 3 stars */
+                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 3;
+                popupCard.setVisibility(View.INVISIBLE);
+                popupRatingMessage.setVisibility(View.INVISIBLE);
+                putRecipeRating();
+            }
+        });
+        /* click listener on rate4 button pressed */
+        rate4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* when rate button is pressed, use Volley request to rate the recipe at 4 stars */
+                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 4;
+                popupCard.setVisibility(View.INVISIBLE);
+                popupRatingMessage.setVisibility(View.INVISIBLE);
+                putRecipeRating();
+            }
+        });
+        /* click listener on rate5 button pressed */
+        rate5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* when rate button is pressed, use Volley request to rate the recipe at 5 stars */
+                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 5;
+                popupCard.setVisibility(View.INVISIBLE);
+                popupRatingMessage.setVisibility(View.INVISIBLE);
+                putRecipeRating();
+            }
+        });
+        /* click listener on cancelRate button pressed */
+        cancelRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupCard.setVisibility(View.INVISIBLE);
+                popupRatingMessage.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
 
     /**
      * Volley PUT request to PUT user's new recipe rating to the server
@@ -166,122 +299,8 @@ public class ViewRecipeActivity extends AppCompatActivity {
     }
 
     /**
-     * Handler for when an option from the toolbar is selected
-     *
-     * @param item The menu item that was selected.
-     * @return true if applicable option is selected
+     * TODO - add documentation get Recipe
      */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection.
-        int itemId = item.getItemId();
-        if (itemId == R.id.view_options_save_recipe) {
-            saveRecipe();
-            return true;
-        } else if (itemId == R.id.view_options_comments) {
-            // TODO - implement new activity to view comments
-            return true;
-        } else if (itemId == R.id.view_options_rate) {
-            rateRecipe();
-            return true;
-        } else if (itemId == R.id.view_options_view_author) {
-            // TODO - implement view author
-            return true;
-        } else if (itemId == R.id.view_options_chat_with_author) {
-            // TODO - implement chat with author
-            return true;
-          // need this for later viewing and making comments, updating recipe
-//        } else if (itemId == R.id.profile_options_logout) {
-//            /* when logout button is pressed, clear sharedPreferences (logging user out) and use intent to switch to Entry Activity */
-//            // getting the data which is stored in shared preferences.
-//            SharedPreferences saved_values = getSharedPreferences(getString(R.string.PREF_KEY), Context.MODE_PRIVATE);
-//            //make editor for shared preferences
-//            SharedPreferences.Editor editor = saved_values.edit();
-//            //clear and save shared preferences
-//            editor.clear();
-//            editor.apply();
-//            //go to EntryActivity
-////            startActivity(new Intent(ProfileActivity.this, EntryActivity.class));
-//            return true;
-//
-        } else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void saveRecipe() {
-        String url = URL_SERVER + "users/";
-        // TODO - implement save recipe
-    }
-
-    private void rateRecipe() {
-        popupCard.setVisibility(View.VISIBLE);
-        popupMessage.setVisibility(View.VISIBLE);
-        /* click listener on rate1 button pressed */
-        rate1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* when rate button is pressed, use Volley request to rate the recipe at 1 star */
-                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 1;
-                popupCard.setVisibility(View.INVISIBLE);
-                popupMessage.setVisibility(View.INVISIBLE);
-                putRecipeRating();
-            }
-        });
-        /* click listener on rate2 button pressed */
-        rate2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* when rate button is pressed, use Volley request to rate the recipe at 2 stars */
-                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 2;
-                popupCard.setVisibility(View.INVISIBLE);
-                popupMessage.setVisibility(View.INVISIBLE);
-                putRecipeRating();
-            }
-        });
-        /* click listener on rate3 button pressed */
-        rate3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* when rate button is pressed, use Volley request to rate the recipe at 3 stars */
-                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 3;
-                popupCard.setVisibility(View.INVISIBLE);
-                popupMessage.setVisibility(View.INVISIBLE);
-                putRecipeRating();
-            }
-        });
-        /* click listener on rate4 button pressed */
-        rate4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* when rate button is pressed, use Volley request to rate the recipe at 4 stars */
-                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 4;
-                popupCard.setVisibility(View.INVISIBLE);
-                popupMessage.setVisibility(View.INVISIBLE);
-                putRecipeRating();
-            }
-        });
-        /* click listener on rate5 button pressed */
-        rate5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* when rate button is pressed, use Volley request to rate the recipe at 5 stars */
-                SPECIFIC_URL_RATING = URL_SERVER + "recipes/" + recipeId + "/rate/" + 5;
-                popupCard.setVisibility(View.INVISIBLE);
-                popupMessage.setVisibility(View.INVISIBLE);
-                putRecipeRating();
-            }
-        });
-        /* click listener on cancelRate button pressed */
-        cancelRate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                popupCard.setVisibility(View.INVISIBLE);
-                popupMessage.setVisibility(View.INVISIBLE);
-            }
-        });
-    }
-
     private void getRecipe() {
         String url = URL_SERVER + "recipes/" + recipeId;
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
@@ -296,6 +315,11 @@ public class ViewRecipeActivity extends AppCompatActivity {
         VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 
+    /**
+     * TODO - add documentation updateUI
+     *
+     * @param response JSONObject containing the information for a recipe
+     */
     private void updateUI(JSONObject response) {
         try {
             Toolbar toolbar = (Toolbar) findViewById(R.id.view_toolbar);
@@ -315,11 +339,5 @@ public class ViewRecipeActivity extends AppCompatActivity {
         } catch (Exception e) {
             Log.e("UpdateUI", "Error parsing response", e);
         }
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.view_menu, menu);
-        return true;
     }
 }
