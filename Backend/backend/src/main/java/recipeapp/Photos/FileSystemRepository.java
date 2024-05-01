93d144b24a6627d9974eb83e3c8d1a8caec09499
David Borucki <02davidb@gmail.com>
2024-05-01 07:35:02 -0500

    changed dir for file saving to work on server

--- Backend/backend/src/main/java/recipeapp/Photos/FileSystemRepository.java
+++ Backend/backend/src/main/java/recipeapp/Photos/FileSystemRepository.java
@@ -3,6 +3,7 @@ package recipeapp.Photos;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Repository;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,7 +14,8 @@ import java.util.Date;
 @Repository
 public class FileSystemRepository {
 
-    String RESOURCES_DIR = FileSystemRepository.class.getResource("/").getPath();
+    //String RESOURCES_DIR = FileSystemRepository.class.getResource("/").getPath();
+    String RESOURCES_DIR = System.getProperty("user.home") + File.separator + "dborucki" + File.separator + "images";
 
     String save(byte[] content, String imageName) throws Exception {
         Path newFile = Paths.get(RESOURCES_DIR + new Date().getTime() + "-" + imageName);
